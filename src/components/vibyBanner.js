fad281f4a08d77d792cf0aa92f4dc75a1dc9b02e
Abel Najih <najihabdelhadi@gmail.com>
2024-03-11 21:20:15 +0200

    react setup

---
+++ src/components/vibyBanner.js
@@ -0,0 +1,12 @@
+
+
+export default function Carousel({ radius = 1.4, count = 8 }) {
+    return Array.from({ length: count }, (_, i) => (
+      <Card
+        key={i}
+        url={`/img${Math.floor(i % 10) + 1}_.jpg`}
+        position={[Math.sin((i / count) * Math.PI * 2) * radius, 0, Math.cos((i / count) * Math.PI * 2) * radius]}
+        rotation={[0, Math.PI + (i / count) * Math.PI * 2, 0]}
+      />
+    ))
+  }
